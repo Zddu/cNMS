438faec652f5ff86f7fb6a202a2bf652fdc32747
zengyan <zengyan.zdde@bytedance.com>
2022-02-15 16:01:24 +0800

    feat: mibs

---
+++ src/monitor/utils/MibParser.ts
@@ -0,0 +1,44 @@
+const mibparser = require('./mib');
+
+interface parserProps {
+  Import: (filename: string) => void;
+  Serialize: () => void;
+  Modules: { [key: string]: any };
+}
+const BASE_MODULES = [
+  'RFC1155-SMI',
+  'RFC1158-MIB',
+  'RFC-1212',
+  'RFC1213-MIB',
+  'SNMPv2-SMI',
+  'SNMPv2-CONF',
+  'SNMPv2-TC',
+  'SNMPv2-MIB',
+];
+class MibParser {
+  private parser: parserProps;
+
+  constructor() {
+    this.parser = mibparser();
+    this.loadBaseModules();
+  }
+
+  private loadBaseModules() {
+    BASE_MODULES.forEach(v => {
+      this.parser.Import('src/monitor/basemibs/' + v + '.mib');
+    });
+    this.parser.Serialize();
+  }
+
+  loadFromFile(filename: string) {
+    this.parser.Import(filename);
+    this.parser.Serialize();
+  }
+
+  getModule(moduleName: string) {
+    console.log(Object.keys(this.parser.Modules));
+    return this.parser.Modules[moduleName];
+  }
+}
+
+export default MibParser;
