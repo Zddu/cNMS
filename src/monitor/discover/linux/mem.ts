d6bf8e16324334a114c990343016b6db6168e099
Zddu <1501815524@qq.com>
2022-02-27 23:34:58 +0800

    feat: 新建内存处理文件

---
+++ src/monitor/discover/linux/mem.ts
@@ -0,0 +1,51 @@
+import { DeviceType } from './../../types';
+import { snmpNext } from '../../../monitor/utils/snmp-utils';
+import { connect } from '../../../database';
+import { isNumber } from '../../../common';
+
+const memTotalReal = [
+  '1.3.6.1.4.1.2021.4.5', // memTotalReal
+  '1.3.6.1.4.1.2021.4.6', // memAvailable
+  '1.3.6.1.4.1.2021.4.13', // memShared
+  '1.3.6.1.4.1.2021.4.14', // memBuffer
+  '1.3.6.1.4.1.2021.4.15', // memCached
+];
+
+export default async function getMem(devive: DeviceType) {
+  const hr_memory = await snmpNext(devive, memTotalReal);
+  let memUsage = 0;
+  const cBS = hr_memory
+    .map((v, i) => {
+      if (i >= 2) {
+        return v.value;
+      }
+    })
+    .reduce((p, v) => p + Number(v), 0);
+  if (cBS > Number(hr_memory[0].value)) {
+    memUsage =
+      ((Number(hr_memory[0].value) -
+        Number(hr_memory[1].value) -
+        Number(hr_memory[3].value) -
+        Number(hr_memory[4].value) +
+        Number(hr_memory[2].value)) *
+        100) /
+      Number(hr_memory[0].value);
+  } else {
+    memUsage =
+      ((Number(hr_memory[0].value) -
+        Number(hr_memory[1].value) -
+        Number(hr_memory[3].value) -
+        Number(hr_memory[4].value)) *
+        100) /
+      Number(hr_memory[0].value);
+  }
+  if (isNumber(memUsage)) {
+    const memModel = {
+      device_id: devive.device_id,
+      mem_usage: memUsage,
+      last_polled: new Date(),
+    };
+    const conn = await connect();
+    await conn.query('insert into cool_mem_rate set ?', [memModel]);
+  }
+}
