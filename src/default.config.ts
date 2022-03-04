e94ccb6251bf2f6ef5b8c5b78792efa69f9f5488
zengyan <zengyan.zdde@bytedance.com>
2022-03-04 13:42:12 +0800

    feat: 新增配置

---
+++ src/default.config.ts
@@ -0,0 +1,36 @@
+export default {
+  discover: {
+    snmp_version: 'v2c',
+    snmp_community_list: ['public'],
+    mode: {
+      arp: true,
+      ospf: false,
+      lldp: false,
+      ip_segment: [],
+      router: false,
+    },
+  },
+  poll: {
+    cpu: {
+      enabled: true,
+      poll_cron: '*/1 * * * *', // 1分钟
+    },
+    mem: {
+      enabled: true,
+      poll_cron: '*/1 * * * *', // 1分钟
+    },
+    disk: {
+      enabled: true,
+      poll_cron: '*/5 * * * *', // 5分钟
+    },
+    interface: {
+      enabled: true,
+      poll_cron: '*/5 * * * *', // 5分钟
+    },
+    flow: {
+      enabled: true,
+      poll_cron: '*/1 * * * *', // 1分钟
+    },
+  },
+  supplier_name_list: ['Linux', 'Windows', 'Ruijie', 'Cisco'],
+};
