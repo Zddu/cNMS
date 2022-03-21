5bb2f992f8b7bb6eed6a813fb1d83db41331b3e0
Zddu <1501815524@qq.com>
2022-03-21 15:51:18 +0800

    feat: 完成ssh密钥存储

--- src/monitor/ssh/handle-ssh.ts
+++ src/monitor/ssh/handle-ssh.ts
@@ -1,17 +1,15 @@
 import { connect } from '../../database';
 import { Request } from 'express';
+import squel from 'squel';
 const { Client } = require('ssh2');
 const conn = new Client();
 let termCols;
 let termRows;
-let sshConfig: any;
 
 export const handleSSH = (ws, req: Request) => {
-  ws.on('message', msg => {
+  ws.on('message', async msg => {
     try {
-      console.log('msg', msg);
-      // msg = JSON.parse(msg);
-      eventHandle(msg);
+      await eventHandle(msg);
     } catch (error) {
       ws.send(JSON.stringify({ event: 'error', data: error.message }));
     }
@@ -19,6 +17,8 @@ export const handleSSH = (ws, req: Request) => {
   try {
     conn
       .once('error', err => {
+        console.log('shell err level', err.level);
+        console.log('shell err message', err.message);
         if (err && err.level === 'client-authentication') {
           ws.send(JSON.stringify({ event: 'reauth', data: err.message }));
         } else {
@@ -67,25 +67,47 @@ export const handleSSH = (ws, req: Request) => {
   }
 };
 
-function eventHandle(data: string) {
+async function eventHandle(data: string) {
   const wsData = JSON.parse(data || '{}') as { event: string; data: any };
   console.log('wsData', wsData);
   switch (wsData.event) {
     case 'auth':
-      sshConfig = {
+      const sshConfig = {
         host: wsData.data.ip,
         port: wsData.data.port,
         username: wsData.data.username,
         password: wsData.data.password,
         tryKeyboard: true,
       };
-      conn.connect({
-        host: wsData.data.ip,
-        port: wsData.data.port,
-        username: wsData.data.username,
-        password: wsData.data.password,
-        tryKeyboard: true,
-      });
+      const sqlSession = await connect();
+      const ssh = (await sqlSession.query('select * from cool_ssh where device_id = ?', [wsData.data.device_id]))[0];
+      const update = squel
+        .update()
+        .table('cool_ssh')
+        .setFields({
+          port: wsData.data.port,
+          username: wsData.data.username,
+          password: wsData.data.password,
+        })
+        .where(`device_id = ?`, wsData.data.device_id)
+        .toString();
+      const insert = squel
+        .insert()
+        .into('cool_ssh')
+        .setFields({
+          device_id: wsData.data.device_id,
+          port: wsData.data.port,
+          username: wsData.data.username,
+          password: wsData.data.password,
+        })
+        .toString();
+      if (!ssh[0]) {
+        await sqlSession.query(insert);
+      } else {
+        await sqlSession.query(update);
+      }
+      sqlSession.end();
+      conn.connect(sshConfig);
     case 'geometry':
       termCols = wsData.data.cols;
       termRows = wsData.data.rows;
