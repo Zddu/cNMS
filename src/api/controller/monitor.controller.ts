dde8d35f455ac8958047911cf5dadfee25808e2d
Zddu <1501815524@qq.com>
2022-04-05 22:00:35 +0800

    feat: 监控项目添加接口

--- src/api/controller/monitor.controller.ts
+++ src/api/controller/monitor.controller.ts
@@ -1,6 +1,7 @@
 import ErrorCode from '../../consts';
 import { Request, Response } from 'express';
 import GlobalIntercept from '../../globalIntercept';
+import { connect } from '../../database';
 
 export async function createMonitorItem(req: Request, res: Response): Promise<Response | void> {
   try {
@@ -10,9 +11,28 @@ export async function createMonitorItem(req: Request, res: Response): Promise<Re
   }
 }
 
+/**
+ * 返回监控项列表
+ * @param req
+ * @param res
+ */
 export async function getMonitorList(req: Request, res: Response): Promise<Response | void> {
   try {
     res.json();
+  } catch (error) {
+    res.json(new GlobalIntercept().error(ErrorCode.UNKNOWN_EXCEPTION, '服务器错误'));
+  }
+}
+
+/**
+ * 返回监控指标列表
+ */
+export async function getMonitorIndexs(req: Request, res: Response): Promise<Response | void> {
+  try {
+    const conn = await connect();
+    const item = (await conn.query('select * from cool_monitor_index'))[0];
+    res.json(new GlobalIntercept().success(item));
+    conn.end();
   } catch (error) {
     res.json(new GlobalIntercept().error(ErrorCode.UNKNOWN_EXCEPTION, '服务器错误'));
   }
--- src/api/router/monitor.routes.ts
+++ src/api/router/monitor.routes.ts
@@ -1,8 +1,9 @@
-import { createMonitorItem } from '../controller/monitor.controller';
+import { createMonitorItem, getMonitorIndexs, getMonitorList } from '../controller/monitor.controller';
 import { Router } from 'express';
 
 const monitorRouter = Router();
 
-monitorRouter.route('/').post(createMonitorItem).get();
+monitorRouter.route('/').post(createMonitorItem).get(getMonitorList);
+monitorRouter.route('/index').get(getMonitorIndexs);
 
 export default monitorRouter;
