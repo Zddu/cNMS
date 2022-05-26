7257316948a17e0d52065d73326d4973a80059bd
Zddu <1501815524@qq.com>
2022-05-26 18:56:21 +0800

    feat: trap

---
+++ src/monitor/trap.ts
@@ -0,0 +1,33 @@
+import { isObj } from "common";
+import { connect } from "database";
+
+const snmp = require('net-snmp');
+
+const options = {
+  port: 162,
+  disableAuthorization: false,
+  includeAuthentication: false,
+  accessControlModelType: snmp.AccessControlModelType.None,
+  address: null,
+  transport: "udp4"
+};
+
+let callback = function (error, notification) {
+  if (error) {
+    console.error(error);
+  } else {
+    hanldeTrap(notification);
+  }
+};
+
+export const receiver = snmp.createReceiver(options, callback);
+
+async function hanldeTrap(notification: any) {
+  const conn = await connect();
+  console.log(JSON.stringify(notification, null, 2));
+  if (isObj(notification) && notification.pdu) {
+    notification.pdu.varbinds.forEach(async v => {
+      await conn.query('insert into cool_trap set ?', [{ v }])
+    });
+  }
+}
